b3e693f97ded9afe4ecee2eda7bf038bf4270ff0
justusadam <development@justusadam.com>
2015-05-15 23:38:58 +0200

    implemented choices, switched to Data.Text
    added ways for user to make choices regarding the package config, like cabal does and elm-package.json output based on those choices
    switched from using mostly Strings to the more efficent Data.Text
    realized system-fileio and system-filepath or whatever is deprecated, swithed to using filepath and directory

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,35 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Main (main) where
 
-import           Control.Applicative       (pure, (<*>))
-import qualified Control.Arrow             as Arrow (first, second)
-import           Control.Exception         (Exception, IOException, catch)
-import           Data.Bool                 (bool)
-import           Data.ByteString           as ByteString (ByteString, hPut)
-import           Data.FileEmbed            (embedFile)
-import           Data.List                 (intercalate)
-import           Data.Maybe                (fromMaybe)
-import           Filesystem                (createTree, getWorkingDirectory,
-                                            isDirectory, isFile)
-import           Filesystem.Path.CurrentOS as Path (FilePath, decodeString,
-                                                    encodeString, (</>))
-import           Prelude                   hiding (FilePath)
-import qualified System.Environment        as Environment (getArgs)
-import           System.IO                 (IOMode (WriteMode), withFile)
-import           Text.JSON                 as JSON (JSON, JSObject,
-                                                    JSValue (JSArray, JSObject),
-                                                    makeObj, showJSON,
-                                                    toJSObject)
+import           Control.Applicative      (pure, (<*>))
+import qualified Control.Arrow            as Arrow (first, second)
+import           Control.Exception        (Exception, IOException, catch)
+import           Data.Aeson               as Aeson (ToJSON, Value, object,
+                                                    toJSON, (.=))
+import           Data.Aeson.Encode.Pretty (encodePretty)
+import           Data.Bool                (bool)
+import qualified Data.ByteString          as ByteString (ByteString, hPut,
+                                                         putStrLn)
+import qualified Data.ByteString.Lazy     as LBS (hPut)
+import           Data.FileEmbed           (embedFile)
+import           Data.List                (intercalate)
+import           Data.Maybe               (fromMaybe)
+import           Data.Text                as Text (Text, append, intercalate,
+                                                   pack, splitOn, unpack)
+import           Data.Text.IO             as TextIO (getLine, putStrLn)
+import           Data.Traversable         (sequenceA)
+import           Prelude                  hiding (getLine, putStrLn)
+import           System.Directory         (createDirectoryIfMissing,
+                                           doesDirectoryExist, doesFileExist,
+                                           getCurrentDirectory)
+import qualified System.Environment       as Environment (getArgs)
+import           System.FilePath          (takeBaseName, (</>))
+import           System.IO                (IOMode (WriteMode), withFile)
 
 
-type Result = Either String ()
+type Result = Either Text ()
 
 
 data CmdArgs = CmdArgs {
@@ -39,47 +45,88 @@ data Version = Version {
 
 
 data UserDecisions = Default {
+    projectName  :: Text,
     sourceFolder :: FilePath,
     version      :: Version,
-    summary      :: String,
-    repository   :: String,
-    license      :: String
+    summary      :: Text,
+    repository   :: Text,
+    license      :: Text,
+    elmVersion   :: Text
   }
 
 
+data ElmPackage = ElmPackage {
+    pkgVersion        :: Version,
+    pkgSummary        :: Text,
+    pkgRepository     :: Text,
+    pkgLicense        :: Text,
+    pkgDependencies   :: Aeson.Value,
+    pkgExposedModules :: [Text],
+    pkgElmVersion     :: Text
+  }
+
+
+instance Aeson.ToJSON ElmPackage where
+  toJSON = object . sequenceA [
+    ("version" .=)            . show . pkgVersion
+    , ("summary" .=)          . pkgSummary
+    , ("repository" .=)       . pkgRepository
+    , ("license" .=)          . pkgLicense
+    , ("dependencies" .=)     . pkgDependencies
+    , ("exposed-modules" .=)  . pkgExposedModules
+    , ("elm-version" .=)      . pkgElmVersion
+    ]
+
+
 instance Show Version where
-  showsPrec p (Version ma mi fi) = shows ma . showChar '.' . shows mi . showChar '.' . shows fi
+  showsPrec p (Version ma mi fi) =
+    shows ma . showChar '.' . shows mi . showChar '.' . shows fi
 
 
-instance Read Version where
-  readsPrec p s =
-    -- this is supposed to do read "1.2.3" => Version 1 2 3
-    [(Version ma mi fi, r) |
-      (ma, '#':r2) <- reads s :: [(Int, String)],
-      (mi, '#':r3) <- reads r2 :: [(Int, String)],
-      (fi, r) <- reads r3 :: [(Int, String)]
-      ]
+-- instance Read Version where
+--   readsPrec p s =
+--     -- this is supposed to do read "1.2.3" => Version 1 2 3
+--     [(Version ma mi fi, r) |
+--       (ma, '.':r2) <- reads s :: [(Int, String)],
+--       (mi, '.':r3) <- reads r2 :: [(Int, String)],
+--       (fi, r) <- reads r3 :: [(Int, String)]
+--       ]
     -- previous version (also does not work)
     -- [(Version ma mi fi, r) |
     --   (ma, '.', mi, '.', fi) <- reads s
     --   ]
 
 
-standardDirectories = map decodeString [
+standardDirectories = [
     "elm-stuff"
   ]
 
 
-standardFiles = map (Arrow.first decodeString) [
-    ("elm-package.json", Just $(embedFile "resources/elm-package.json")),
+standardFiles = [
     ("README.md", Nothing),
     ("LICENSE", Nothing)
   ]
 
 
-standardSourceFiles = map (Arrow.first decodeString) [
+standardSourceFiles = [
     ("Main.elm", Just $(embedFile "resources/Main.elm"))
   ]
+
+
+defaultProjectVersion = Version 1 0 0
+
+
+defaultElmVersion = "0.15.0 <= v < 0.16.0"
+
+
+defaultLicenses = [
+    "None",
+    "BSD3",
+    "LGPL3",
+    "LGPL2",
+    "MIT",
+    "Apache"
+  ] :: [Text]
 
 
 {-
@@ -97,45 +144,71 @@ sourceFolders = [
   ]
 
 
-elmPackageJSON :: UserDecisions -> JSValue
-elmPackageJSON d@(Default {sourceFolder=sourceFolder, version=version, summary=summary, license=license, repository=repository}) =
-  makeObj $ (
-    ("dependencies", JSObject $ toJSObject $ map (Arrow.second showJSON) [
-      ("elm-lang/core", "2.0.0 <= v < 3.0.0"),
-      ("evancz/elm-html", "3.0.0 <= v < 4.0.0")
-      ]
-    )
-  ) : (map (Arrow.second showJSON) [
-    ("version", show version),
-    ("summary", summary),
-    ("repository", repository),
-    ("license", license),
-    ("elm-version", "0.15.0 <= v < 0.16.0")
-  ]) ++ (map (Arrow.second showJSON) [
-    ("source-directories", [
-      showJSON $ encodeString sourceFolder
-      ]),
-    ("exposed-modules", [])
-    ]
-  )
+elmConfigName = "elm-package.json" :: FilePath
+
+
+makePackage :: UserDecisions -> ElmPackage
+makePackage = pure ElmPackage
+  <*> version
+  <*> summary
+  <*> repository
+  <*> license
+  <*> const (object [])
+  <*> const []
+  <*> elmVersion
+
 
 
 emptyDecisions :: UserDecisions
 emptyDecisions =
   Default {
     summary       = "",
-    repository    = "https://github.com/JustusAdam/elm-init.git",
+    repository    = "",
     version       = Version 1 0 0,
-    license       = "BSD3",
-    sourceFolder  = decodeString "src"
+    license       = "",
+    sourceFolder  = "src",
+    projectName   = "",
+    elmVersion    = defaultElmVersion
   }
+
+
+getUserDecisions :: FilePath -> IO UserDecisions
+getUserDecisions wd =
+  pure Default
+  <*> askChoicesWithOther "project name?" 0 (const True) [pack $ takeBaseName wd]
+  <*> (fmap ((wd </>).unpack) (askChoicesWithOther "choose a source folder name" 0 (const True) (map pack sourceFolders)))
+  <*> fmap versionFromString (askChoicesWithOther "initial project version?" 0 (const True) [pack $ show defaultProjectVersion])
+  <*> (putStrLn "a quick summary" >> getLine)
+  <*> (putStrLn "project repository url" >> getLine)
+  <*> askChoicesWithOther "choose a license" 0 (const True) defaultLicenses
+  <*> askChoicesWithOther "select the elm-version" 0 (const True) [defaultElmVersion]
 
 
 enumerate :: Int -> [a] -> [(Int,a)]
 enumerate from l = zip [from..(length l)] l
 
 
-askChoices :: String -> Int -> [String] -> IO String
+splitL :: Eq a => a -> [a] -> [[a]]
+splitL _ [] = []
+splitL se l = reverse $ foldl (helper se) [[]] l
+  where
+    helper :: Eq a => a -> [[a]] -> a -> [[a]]
+    helper se [] e = helper se [[]] e
+    helper se acc@(x:xs) e
+      | se == e   = []:acc
+      | otherwise = (e:x):xs
+
+
+versionFromString :: Text -> Version
+versionFromString s
+  | length sp /= 3  = error "Parse failed"
+  | otherwise       = Version ma mi fi
+  where
+    sp@(ma:mi:fi:[]) = map (read.unpack) (splitOn "." s) :: [Int]
+
+
+
+askChoices :: Text -> Int -> [Text] -> IO Text
 askChoices m s l = askChoices' m s l >>= (\i -> return $ l !! i)
 
 
@@ -147,24 +220,27 @@ getEither x =
     handler = const . return
 
 
-askChoices' :: String -> Int -> [String] -> IO Int
+askChoices' :: Text -> Int -> [Text] -> IO Int
 askChoices' message selected choices = do
-  putStrLn message
-  let (l1, l2) = splitAt selected choices
-  let (selectedElem : l2tail) = l2
-  let out = intercalate "\n" (normFormat 1 l1 ++ (selectedFormat selected selectedElem : normFormat (selected + 1) l2tail))
-
+  TextIO.putStrLn message
   ask out
 
   where
-    enumF x = show x ++ " )  "
-    enumFn = ("    " ++).enumF
-    enumFs = ("  * " ++).enumF
-    normFormat f l = map (uncurry (++) . Arrow.first enumFn) $ enumerate f l
-    selectedFormat x y = ((++ y).enumFs) x
+    (l1, (selectedElem : l2tail)) = splitAt selected choices
+    out =
+      Text.intercalate
+        "\n"
+        (normFormat 1 l1
+        ++ (selectedFormat selected selectedElem : normFormat (selected + 1) l2tail))
+
+    enumF x = append (pack $ show x) " )  "
+    enumFn = (append "    ").enumF
+    enumFs = (append "  * ").enumF
+    normFormat f l = map ((uncurry append) . Arrow.first enumFn) $ enumerate f l
+    selectedFormat x y = ((`append` y).enumFs) x
 
     ask out = do
-          putStrLn out
+          TextIO.putStrLn out
           -- apparently using putStr here doe not print the full string but
           -- omits the last line ... buffering?
           i <- getEither selected
@@ -172,12 +248,12 @@ askChoices' message selected choices = do
           if i <= length choices then
             return i
           else do
-            putStrLn "invalid choice, please choose again"
+            TextIO.putStrLn "invalid choice, please choose again"
             ask out
 
 
-askChoicesWithOther :: String -> Int -> [String] -> IO String
-askChoicesWithOther m s l = do
+askChoicesWithOther :: Text -> Int -> (Text -> Bool) -> [Text] -> IO Text
+askChoicesWithOther m s verifier l = do
   i <- askChoices' m s (l ++ ["other (specify)"])
   if i == length l then
     getAlternative
@@ -185,39 +261,39 @@ askChoicesWithOther m s l = do
     return $ l !! i
 
   where
-    verifyValidity = const True
     getAlternative = do
-      putStrLn "please enter an alternative"
+      TextIO.putStrLn "please enter an alternative"
       s <- getLine
-      if verifyValidity s then
+      if verifier s then
         return s
       else
+        TextIO.putStrLn "Invalid input, plese enter again" >>
         getAlternative
 
 
 exists :: FilePath -> IO Bool
 exists f = do
-  isF   <- isFile f
-  isDir <- isDirectory f
+  isF   <- doesFileExist f
+  isDir <- doesDirectoryExist f
   return $ isF || isDir
 
 
-mkFiles :: [(FilePath, Maybe ByteString)] -> IO [Result]
+mkFiles :: [(FilePath, Maybe ByteString.ByteString)] -> IO [Result]
 mkFiles = mapM (uncurry mkFile)
 
 
-mkFile :: FilePath -> Maybe ByteString -> IO Result
+mkFile :: FilePath -> Maybe ByteString.ByteString -> IO Result
 mkFile name defaultFile = exists name >>=
   bool
-    (return $ Left $ "file " ++ encodeString name ++ " already exists")
     (
-      System.IO.withFile
-        (encodeString name)
-        WriteMode
+      withFile
+        name
+        System.IO.WriteMode
         (\h -> maybe (return ()) (ByteString.hPut h) defaultFile)
       >>
       return (Right ())
       )
+    (return $ Left $ "file " `append` pack name `append` " already exists")
 
 
 mkSourceFiles :: FilePath -> IO [Result]
@@ -225,7 +301,11 @@ mkSourceFiles sourceFolder = mkFiles $ map (Arrow.first (sourceFolder </>)) stan
 
 
 mkDirs :: FilePath -> [FilePath] -> IO ()
-mkDirs wd = mapM_ ( createTree . (wd </>))
+mkDirs wd = mapM_ ((createDirectoryIfMissing True) . (wd </>))
+
+
+writeConf :: FilePath -> UserDecisions -> IO ()
+writeConf wd dec = withFile (wd </> elmConfigName) System.IO.WriteMode (`LBS.hPut` (encodePretty $ makePackage dec))
 
 
 getCmdArgs :: IO CmdArgs
@@ -234,9 +314,9 @@ getCmdArgs =
     (\args ->
       if length args > 1
         then
-          return $ decodeString $ head args
+          return $ head args
         else
-          getWorkingDirectory
+          getCurrentDirectory
     ) >>=
         \wd -> return CmdArgs { workingDirectory = wd }
 
@@ -247,19 +327,17 @@ main = do
 
   wd        <- fmap workingDirectory getCmdArgs
 
-  srcFolder <- fmap ((wd </>).decodeString) (askChoicesWithOther "choose a source folder name" 0 sourceFolders)
+  decisions <- getUserDecisions wd
 
-  projectName <- askChoicesWithOther "project name?" 0 [encodeString wd]
-
-  -- putStrLn srcFolder
-
-  mkDirs wd (srcFolder : standardDirectories)
+  mkDirs wd (sourceFolder decisions : standardDirectories)
   resStatic <- mkFiles standardFiles
 
-  resSource <- mkSourceFiles srcFolder
+  resSource <- mkSourceFiles $ sourceFolder decisions
+
+  writeConf wd decisions
 
   mapM_ (\r ->
       case r of
         Right _       -> return ()
-        Left message  -> putStrLn message
+        Left message  -> TextIO.putStrLn message
     ) (resStatic ++ resSource)
